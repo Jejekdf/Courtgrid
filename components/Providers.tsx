b8b7a025fa74e0a6010d8503527035b28f589ae8
Jejekdf <maulanarandi531@gmail.com>
2026-08-16 20:57:29 +0700

    fix(providers): remove next-themes theme provider

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "next-themes";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { MotionConfig } from "framer-motion";
@@ -12,13 +11,11 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => makeQueryClient());
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} forcedTheme="light">
-      <MotionConfig reducedMotion="user">
-        <QueryClientProvider client={queryClient}>
-          <SessionProvider>{children}</SessionProvider>
-          {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
-        </QueryClientProvider>
-      </MotionConfig>
-    </ThemeProvider>
+    <MotionConfig reducedMotion="user">
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider>{children}</SessionProvider>
+        {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
+      </QueryClientProvider>
+    </MotionConfig>
   );
 }
